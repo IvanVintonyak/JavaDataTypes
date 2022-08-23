e203e7d472aa9de3acae39c8a302b3ee1d084442
Ivan <vintonyaki1990@gmail.com>
2022-08-23 23:44:44 +0300

    Main commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[]args)
+    {
+        System.out.println();
+        printMessage("Hello I am John, I am ... " );
+        printSum();
+
+    }
+    public static void printMessage(String s)
+    {
+        String string = "s";
+        System.out.println(s);
+    }
+
+    public static void printSum()
+    {
+     int a =30;
+     int b =2;
+        System.out.println(a+b);
+    }
+}
+
+
