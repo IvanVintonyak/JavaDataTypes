623e57549fe5316b101876ffcd336cc63eaea8d9
Ivan <vintonyaki1990@gmail.com>
2022-08-25 23:17:25 +0300

    Add some fixes

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
-    public static void main (String[]args)
-    {
+    public static void main(String[] args) {
         printMessage("Hello");
-        printSum(30,2);
+        printSum(30, 2);
     }
+
     public static void printMessage(String s) {
         System.out.print(s + "I am John, I am ...");
     }
 
     public static void printSum(int a, int b) {
-        System.out.print(a+b);
-
+        System.out.print(a + b);
     }
 
-    }
+}
 
 
 
