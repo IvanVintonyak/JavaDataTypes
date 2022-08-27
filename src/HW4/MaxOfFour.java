210a48a91cf6e1f3a7fe93b0418e66c98e665f53
Ivan <vintonyaki1990@gmail.com>
2022-08-27 14:56:09 +0300

    agg Max of four

---
+++ src/HW4/MaxOfFour.java
@@ -0,0 +1,28 @@
+package HW4;
+
+import java.util.Scanner;
+
+public class MaxOfFour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter four numbers:");
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        int three = scanner.nextInt();
+        int four = scanner.nextInt();
+        printMaxOfFour(one, two, three, four);
+        scanner.close();
+    }
+
+    private static void printMaxOfFour(int one, int two, int three, int four) {
+        if (!(one < two || one < three || one < four)) {
+            System.out.println(one + " is the biggest variable");
+        } else if (!(two < three || two < four)) {
+            System.out.println(two + " is the biggest variable");
+        } else if (!(three < four)) {
+            System.out.println(three + " is the biggest variable");
+        } else {
+            System.out.println(four + " is the biggest variable");
+        }
+    }
+}
