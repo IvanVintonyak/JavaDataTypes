36285792250e7152f832021e38f0816348ffbf5a
Ivan <vintonyaki1990@gmail.com>
2022-08-27 14:57:23 +0300

    MO2

--- src/HW4/MinOfTwo.java
+++ src/HW4/MinOfTwo.java
@@ -2,7 +2,7 @@ package HW4;
 
 import java.util.Scanner;
 
-public class MainOfTwo {
+public class MinOfTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two numbers");
