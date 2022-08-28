b7d23f86a9231bc87612c8dcd24d83414e3c7a58
Ivan <vintonyaki1990@gmail.com>
2022-08-28 10:15:14 +0300

    Add some fixes

---
+++ src/For_loop/EvenNumbers.java
@@ -0,0 +1,14 @@
+package For_loop;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        /*
+    using for loop
+    print even number from 1 to 100 include
+    from new line
+     */
+        }
+    }
+}
