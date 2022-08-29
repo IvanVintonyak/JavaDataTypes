e474d4e5af31675cb9efdc14afc1ee6c6b3e6569
Ivan <vintonyaki1990@gmail.com>
2022-08-29 14:50:38 +0300

    Add some fixes

--- src/For_loop/EvenNumbers.java
+++ src/For_loop/EvenNumbers.java
@@ -2,8 +2,9 @@ package For_loop;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        for (int i = 2; i <= 100; i = i + 2) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
         /*
     using for loop
     print even number from 1 to 100 include
